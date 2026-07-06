17d2a6b4faca29075fe610238f9633b1517efdb9
harsha balakrishnan <harsha16balakrishnan@proton.me>
2026-07-07 00:34:45 +0530

    fix(ci): update Homebrew cask checksum for v1.0.0 release zip

--- Casks/lively.rb
+++ Casks/lively.rb
@@ -1,6 +1,6 @@
 cask "lively" do
   version "1.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "c2751aaeebf5aa11997e535d850e8e693cad9fe88760719d55eeb39ccd2a1483"
 
   url "https://github.com/harshabala/Lively/releases/download/v1.0.0/Lively-1.0.0-macOS.zip"
   name "Lively"
